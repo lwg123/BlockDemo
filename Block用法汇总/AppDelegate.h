ab624d670ff5406d57c26d72aff83e22883a5bf9
李伟光 <weiguang.li@changhong.com>
2018-04-14 22:32:33 +0800

    init

---
+++ Block用法汇总/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block用法汇总
+//
+//  Created by weiguang on 2018/4/14.
+//  Copyright © 2018年 weiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Block用法汇总/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Block用法汇总
+//
+//  Created by weiguang on 2018/4/14.
+//  Copyright © 2018年 weiguang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
